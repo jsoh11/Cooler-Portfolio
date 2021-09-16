4ca78ec0e003a5dcc1bd09e6e910dabe5e83e013
Jordan <jordansoh14@gmail.com>
2021-09-16 18:33:07 -0500

    added click function to modal

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 
 const cancelBtn = document.querySelector('#cancel-btn');
 const deleteBtn = document.querySelector('#delete-btn');
+const modal = document.querySelector('.modal');
+const contact = document.querySelector('#contact');
+
+contact.addEventListener('click', () => {
+    modal.classList.add('is-active');
+});
+
+cancelBtn.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+})
